78b13c0426477da5f518e3603a216d567edcffbb
Enzo Ferri <enzo@debian-BULLSEYE-live-builder-AMD64>
2023-07-14 17:40:32 +0200

    chore: update documentation

---
+++ 1.x/scripts/changelog-test.js
@@ -0,0 +1,11 @@
+const { getChangelog } = require('./changelog')
+const fs = require('fs')
+
+;(async () => {
+  const changelog = await getChangelog()
+
+  fs.writeFile('changelog.txt', changelog, function (err) {
+    if (err) throw err
+    console.log('changelog.txt has been created successfully.')
+  })
+})()
---
+++ 1.x/scripts/create-release-attachment.sh
@@ -0,0 +1,3 @@
+mkdir ./dist/zip
+cd ./dist
+zip -r ./zip/bootstrap-lombardia.zip . -x zip/\*
