8fa6e2b4b20c0f904ddb931f7d8ad20beb183879
Enzo Ferri <enzo@debian-BULLSEYE-live-builder-AMD64>
2023-11-22 15:02:39 +0100

    chore(release): 2.0.1 :tada:

--- src/js/it25_version.js
+++ src/js/it25_version.js
@@ -1,5 +1,5 @@
 // bootstrap lombardia version variable
 // useful to check for the current version
 // eslint-disable-next-line no-unused-vars
-const BOOTSTRAP_LOMBARDIA_VERSION = '2.0.0'
+const BOOTSTRAP_LOMBARDIA_VERSION = '2.0.1'
 export default BOOTSTRAP_LOMBARDIA_VERSION
