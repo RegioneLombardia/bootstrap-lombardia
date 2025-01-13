6c45b5ab617e9748dcbf378d192e2500eddc9ca3
Enzo Ferri <enzo@debian-BULLSEYE-live-builder-AMD64>
2025-01-13 14:26:39 +0100

    chore(release): 2.5.2 :tada:

--- src/js/it25_version.js
+++ src/js/it25_version.js
@@ -1,5 +1,5 @@
 // bootstrap lombardia version variable
 // useful to check for the current version
 
-const BOOTSTRAP_LOMBARDIA_VERSION = '2.5.1'
+const BOOTSTRAP_LOMBARDIA_VERSION = '2.5.2'
 export default BOOTSTRAP_LOMBARDIA_VERSION
