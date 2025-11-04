6f983a5003e434f9e2c6b1cb53fc52e9a02b5f10
EnzoFerri <EnzoFerri@users.noreply.github.com>
2025-11-04 10:26:18 +0000

    chore: update documentation

--- compile-docs.sh
+++ compile-docs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-git clone https://github.com/RegioneLombardia/bootstrap-lombardia.git
-
-cd bootstrap-lombardia
-
-git checkout 1.x
-
-npm i && bundle install && npm run build && JEKYLL_ENV=production bundle exec jekyll build --config _config.yml,_config_production.yml
-
-cd ..
-
-mv bootstrap-lombardia/_site ./_site/1.x
+# git clone https://github.com/RegioneLombardia/bootstrap-lombardia.git
+#
+# cd bootstrap-lombardia
+#
+# git checkout 1.x
+#
+# npm i && bundle install && npm run build && JEKYLL_ENV=production bundle exec jekyll build --config _config.yml,_config_production.yml
+#
+# cd ..
+#
+# mv bootstrap-lombardia/_site ./_site/1.x
